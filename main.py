3b1e7ad806016e05746466a0573a62bac0e19276
Rully Lukman <rully.lukman@gmail.com>
2023-12-06 20:01:08 +0700

    add CORS

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from users.routes import user_router as user_router
 from auth.routes import router as auth_router
 from core.security import JWTAuth
 from starlette.middleware.authentication import AuthenticationMiddleware
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = ["*"]
 
 app = FastAPI()
 app.include_router(guest_router)
@@ -13,6 +16,13 @@ app.include_router(auth_router)
 
 # middleware 
 app.add_middleware(AuthenticationMiddleware, backend=JWTAuth())
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
 
 @app.get('/')
 def health_check():
